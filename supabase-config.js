bd615f6a76bb44dd297049cdba10e885dca60c92
EvgeniGoshev <evgenigosev66@gmail.com>
2026-07-10 14:52:49 +0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+window.FIRST_AID_SUPABASE_CONFIG = {
+  url: "https://jomtoqovtvbcipzwecgd.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvbXRvcW92dHZiY2lwendlY2dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2Nzc0OTksImV4cCI6MjA5OTI1MzQ5OX0.Q-AAKXMH8FFKWLoxjUa-B3FNvyaYbRIXcvarlxwWDmM"
+};
